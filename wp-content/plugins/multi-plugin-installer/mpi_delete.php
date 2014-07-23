bcd478d3172c9af444e840fd6ef770648832c5a8
munchkin <munchkin@do>
2014-07-22 20:14:40 -0400

    plugin update and removed searchreplace

--- wp-content/plugins/multi-plugin-installer/mpi_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file_path=$_REQUEST['filepath'].$_REQUEST['filename'];
-mpi_delfile($file_path);
-function mpi_delfile($file_path){
-	@unlink($file_path);
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
-}
-?>
